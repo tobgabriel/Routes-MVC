6193659e05ccc8899b8728eeb46171ce2463e8cf
Tiago Gabriel <tobgabriel@gmail.com>
2021-12-10 12:02:28 -0300

    v 0.1

---
+++ app/public/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', true);
+    require dirname(__DIR__,1). '/vendor/autoload.php';
+
+    $rt=new app\Router\Router();
+    
+    $rt->get('/',function(){
+    	echo "Hello World";    
+    });
+
+    
--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,5 @@
 <?php
-
-	require_once ''../bootstrap.php';'
+	require_once 'bootstrap.php';
+	$method=$_SERVER['REQUEST_METHOD'];
+	$route = $_SERVER['REQUEST_URI'];
+	$rt->find($method,$route);
